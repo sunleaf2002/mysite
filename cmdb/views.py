0375045d3dff9d4f1099aa40fca13038ad572ecc
Administrator <sunleaf2002@126.com>
2017-01-21 23:42:06 +0800

    界面输入的数据可以保存到sqlite3数据库里面

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.shortcuts import  HttpResponse
+from cmdb import models
 # Create your views here.
+#user_list=[
+ #  {"user":"jack","pwd":"abc"},
+  #  {"user":"tom","pwd":"ABC"}]
 def index(request):
     #request.post
     #request.get
-    return HttpResponse("hello world")
+    if request.method == "POST":
+        username = request.POST.get("username",None)
+        password = request.POST.get("password",None)
+        #print(username,password)
+   #     temp = {"user":username,"pwd":password}
+    #    user_list.append(temp)
+        models.UserInfo.objects.create(user  =username,pwd = password)
+        user_list = models.UserInfo.objects.all()
+    return render(request,"index.html",{"data":user_list})
